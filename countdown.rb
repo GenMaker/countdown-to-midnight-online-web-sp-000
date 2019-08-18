3ae143c3f7982e28e260d04080c4ffb05290107c
Panela Leung <leungpanela@gmail.com>
2019-08-18 10:50:05 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,12 +1,11 @@
 #write your code here
-def countdown_with_sleep (num)
-  sleep (num)
+def countdown_with_sleep (int)
+  sleep (int)
 end
 
 def countdown (number)
-  countdown_with_sleep(5)
   puts "#{number} SECOND(S)!"
-  while number != 0
+  while number > 0
     puts "#{number} SECOND(S)!"
     number -= 1
   end
