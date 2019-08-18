a0d2633c3ad49eda21a07d37184604af392294fe
Panela Leung <leungpanela@gmail.com>
2019-08-18 10:39:18 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,7 +4,7 @@ def countdown_with_sleep (num)
 end
 
 def countdown (number)
-  countdown_with_sleep
+  countdown_with_sleep(num)
   while number > 0
     puts "#{number} SECOND(S)!"
     number -= 1
