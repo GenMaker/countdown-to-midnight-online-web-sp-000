28d3ca3efde62fcfb01131fd95adf0ddf56b7ab7
Panela Leung <leungpanela@gmail.com>
2019-08-18 10:31:06 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
 #write your code here
 
-def countdown
+def countdown (number)
+while number > 0
+  number -= 1
+end
+ end
+
+def countdown_with_sleep (number)
+  sleep (1)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    countdown(number)
+  end
+   puts "HAPPY NEW YEAR!"
 end
